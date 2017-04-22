590c2af111cd911565dbcdb673c59ff2150f5da5
Manar Mohammed <mkmanar762@gmail.com>
2017-04-21 21:53:57 -0700

    Add files via upload

---
+++ E4_20.java
@@ -0,0 +1,15 @@
+public class E4_20
+{
+    public static void main(String [] args)
+    {
+        System.out.println("    /\\    ");
+        System.out.println("   /  \\   ");
+        System.out.println("  /    \\  ");
+        System.out.println(" /      \\ ");
+        System.out.println(" --------  ");
+        System.out.println("   #  #    ");
+        System.out.println("   #  #    ");
+        System.out.println("   #  #    ");
+    }
+}        
+    
